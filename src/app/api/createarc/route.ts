d9e0990d17f03ce7a98e711f02c1d0a4528acfdf
Ayush0054 <ayush5aprill@gmail.com>
2024-06-02 13:00:36 +0530

    create-arc api

--- src/app/api/createarc/route.ts
+++ src/app/api/createarc/route.ts
@@ -9,7 +9,7 @@ import { currentUser } from "@clerk/nextjs";
 import { redirectToSignIn } from "@clerk/nextjs";
 const prisma = new PrismaClient();
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   try {
     const user = await currentUser();
     const body = await req.json();
@@ -35,44 +35,6 @@ export async function POST(req: Request, res: Response) {
       },
     });
     return NextResponse.json(newarc, { status: 200 });
-  } catch (error) {
-    console.error("Request error", error);
-    return NextResponse.json(
-      { error: "Error retrieving goals" },
-      { status: 500 }
-    );
-  }
-}
-export async function GET(request: Request) {
-  try {
-    const user = await currentUser();
-
-    if (!user) {
-      return redirectToSignIn();
-    }
-    const goals = await prisma.arc.findMany({
-      where: {
-        id: "a1a4e5d8-0b13-4ffd-b221-40b7c67501d3",
-      },
-      include: {
-        profile: {
-          select: {
-            name: true,
-          },
-        },
-        todo: {
-          select: {
-            arcId: true,
-          },
-        },
-        Notes: {
-          select: {
-            arcId: true,
-          },
-        },
-      },
-    });
-    return NextResponse.json(goals, { status: 200 });
   } catch (error) {
     console.error("Request error", error);
     return NextResponse.json(
