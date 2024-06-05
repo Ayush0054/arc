4db15e96a07567562776b4cc7c5f804372d2fd06
Ayush0054 <ayush5aprill@gmail.com>
2024-06-06 00:57:51 +0530

    framer

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -5,22 +5,51 @@ import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import Image from "next/image";
 import { Badge } from "../ui/badge";
-import Link from "next/link";
+import { motion } from "framer-motion";
 
 function Hero() {
+  const FADE_DOWN_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
   return (
-    <div className="  flex  flex-col items-center mt-32">
-      <div className="text-center text-red-900/opacity-20 text-5xl font-medium text-white/80 ">
-        Boost your productivity with Arc
-      </div>
-      <div className="w-[287px] h-[313px] bg-blue-600 rounded-full blur-[300px]"></div>
+    <motion.div
+      initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+    >
+      <div className="  flex  flex-col items-center mt-32">
+        <motion.h1
+          className="text-center font-display text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <div className="text-center text-red-900/opacity-20 text-5xl font-medium text-white/80 ">
+            Boost your productivity with Arc
+          </div>
+        </motion.h1>
+        <div className="w-[287px] h-[313px] bg-blue-600 rounded-full blur-[300px]"></div>
 
-      <Image
-        src={heroImage}
-        className="  -mt-[300px] relative shadow-xl"
-        alt=""
-      />
-    </div>
+        <motion.div
+          className=" text-center md:text-2xl"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          Animation Preview
+          <Image
+            src={heroImage}
+            className="  -mt-[300px] relative shadow-xl"
+            alt=""
+          />
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
 
--- src/components/landing-page/navbar.tsx
+++ src/components/landing-page/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "../ui/button";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-
+import { motion } from "framer-motion";
 interface NavbarProps {
   setAuthModal: (open: boolean) => void;
 }
@@ -11,23 +11,45 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ setAuthModal }) => {
   const { isSignedIn } = useUser();
   const router = useRouter();
-
+  const FADE_DOWN_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
   return (
     <div className=" -mt-[250px] shadow-sm py-2">
-      <div className="flex justify-between items-center">
-        <h1 className="text-5xl font-semibold">Arc</h1>
+      <motion.div
+        initial="hidden"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+      >
+        <motion.div
+          className="text-center font-display text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <div className="flex justify-between items-center">
+            <h1 className="text-5xl font-semibold">Arc</h1>
 
-        <div className="flex gap-4">
-          <Button
-            onClick={() =>
-              isSignedIn ? router.push("/home") : setAuthModal(true)
-            }
-            className="bg-[#0e1d24] hover:bg-[#0e1d24] rounded-2xl"
-          >
-            Get Started
-          </Button>
-        </div>
-      </div>
+            <div className="flex gap-4">
+              <Button
+                onClick={() =>
+                  isSignedIn ? router.push("/home") : setAuthModal(true)
+                }
+                className="bg-[#0e1d24] hover:bg-[#0e1d24] rounded-2xl"
+              >
+                Get Started
+              </Button>
+            </div>
+          </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
